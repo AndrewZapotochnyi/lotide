b033074ef84602a192b48143dd9b1d872d20a560
AndrewZapotochnyi <info@azblockchain.co>
2020-03-31 23:02:56 +0000

    Added assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual,expected)) {
+    console.log(`😎😎😎 Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+  /*
+  if (actual === expected) {
+    console.log(`😎😎😎 Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+  */
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+
+  if (!arrayOne.length || !arrayTwo.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      if (i === arrayOne.length - 1) {
+        return true;
+      } 
+    } else {
+      if (i === arrayOne.length - 1) {
+        return false;
+      } else if (arrayOne[i] === undefined) {
+        return false;
+      }
+    }
+  }
+
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2"]);
+assertArraysEqual([], ["1", "2"]);
