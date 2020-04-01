ebb28d0d6c62a03b7888d48e310b8af5c5cf4479
AndrewZapotochnyi <info@azblockchain.co>
2020-04-01 20:39:15 +0000

    Added letter counter returning object

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+let letters = 'lighthouse in the house';
+
+const countLetters = function(sentence) {
+  results = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (letter in results) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+console.log(countLetters(letters));
+console.log(countLetters("ooooooooooooooooooowww"));
+console.log(countLetters(""));
