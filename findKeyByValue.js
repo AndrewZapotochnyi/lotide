fba11eceb7a15bc567979cd75b23a988af14a559
AndrewZapotochnyi <info@azblockchain.co>
2020-04-01 22:57:37 +0000

    Added findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎😎 Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const findKeyByValue = function(movies, movieName) {
+  for (const genre in movies) {
+    if (movieName === movies[genre]) {
+      return genre;
+    } 
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
